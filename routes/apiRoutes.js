f2a22cb2fffdacae5cc3dbb5329844d545768013
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-05-11 09:44:01 -0400

    worked on route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,20 +7,27 @@ module.exports = (app) =>{
   
     // get request for the notes that have been stored
     app.get("/api/workouts", (req, res) => {
-      db.Workout.find()
-        .then((workouts) => {
-          res.json(workouts);
-        })
-        .catch((err) => {
-          console.log("error == ", err);
-        });
+        db.Workout.find({}).then(databaseWorkout => {
+      
+          databaseWorkout.forEach(workout => {
+              var total = 0;
+              workout.exercises.forEach(e => {
+                  total += e.duration;
+              });
+              workout.totalDuration = total;
+
+          });
+
+          res.json(databaseWorkout);
+      }).catch(err => {
+          res.json(err);
+      });
     });
   
   
     // post notes 
-  
     app.post("/api/workouts/", function(req, res) {
-      db.Workout.create(req.body)
+     db.Workout.create(req.body)
       .then((workouts) => {
         res.json(workouts);
       })
@@ -31,24 +38,30 @@ module.exports = (app) =>{
 
     //uppdate
     app.put("/api/workouts/:id", function(req, res) {
-      db.Workout.create(req.body)
-      .then((workouts) => {
-        res.json(workouts);
-      })
-      .catch((err) => {
-        console.log("error == ", err);
-      });
+
+      db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $push: { exercises: req.body }
+        },
+        { new: true }).then(workout => {
+            res.json(workout);
+        }).catch(err => {
+            res.json(err);
+        });
     });
 
     // get request for the notes that have been stored
     app.get("/api/workouts/range", (req, res) => {
-      db.Workout.find()
-        .then((workouts) => {
-          res.json(workouts);
-        })
-        .catch((err) => {
-          console.log("error == ", err);
-        });
+      
+      db.Workout.find({}).then(workout => {
+        console.log("ALL WORKOUTS");
+        console.log(workout);
+
+        res.json(workout);
+    }).catch(err => {
+        res.json(err);
+    });
     });
 
 };
